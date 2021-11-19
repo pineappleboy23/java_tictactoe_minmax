cf0693140bb6afe113175e0ac5cfc7eb5320ce1c
pineappleboy23 <79547397+pineappleboy23@users.noreply.github.com>
2021-11-18 18:42:38 -0700

    Add files via upload

--- tic_tac_toe_minmax/GameLoop.java
+++ tic_tac_toe_minmax/GameLoop.java
@@ -1,10 +1,11 @@
 package tic_tac_toe_minmax;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameLoop {
 
-    String is_winner_or_tie(Game game_in) {
+    static String is_winner_or_tie(Game game_in) {
         Game.Square eval = game_in.evaluate_position(game_in.board);
         if (eval == Game.Square.T) {
             return "tie no winner";
@@ -21,35 +22,44 @@ public class GameLoop {
         Game game = new Game();
 
         Scanner get_it = new Scanner(System.in);
+        Random rand = new Random();
 
 
-
+        boolean comp_turn = false;
+        if (rand.nextDouble() > .5){
+            comp_turn = true;
+        }
 
         boolean running = true;
         while(running) {
 
 
-            //computer move
-            game.comp_move(Game.Square.O);
+            if (comp_turn) {
+                //computer move
+                game.comp_move(Game.Square.O);
+                comp_turn = !comp_turn;
+            } else{
+                //player move
+                boolean has_moved = false;
 
-            //player move
-            boolean has_moved = false;
+                while (!has_moved) {
+                    game.draw_game();
+                    System.out.println("Enter move in the form '02' ");
+                    String move = get_it.nextLine();
+                    if (game.check_and_do_player_move(move, Game.Square.X)) {
+                        has_moved = !has_moved;
+                        System.out.println("move success");
+                    }
 
-            while (!has_moved) {
-                game.draw_game();
-                System.out.println("Enter move in the form '02' ");
-
-                String move = get_it.nextLine();
-                if (game.check_and_do_player_move(move, Game.Square.X)) {
-                    has_moved = !has_moved;
-                    System.out.println("move success");
                 }
 
+                comp_turn = !comp_turn;
             }
-
-
-
-
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println(is_winner_or_tie(game));
         }
 
 
